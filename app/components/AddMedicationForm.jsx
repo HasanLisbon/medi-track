cea52a05a54c846a834de1faabecc0f1d52fd794
Mahmudul Hasan <35339345+HasanLisbon@users.noreply.github.com>
2025-02-03 12:49:49 +0100

    dropdown for whentotake

--- app/components/AddMedicationForm.jsx
+++ app/components/AddMedicationForm.jsx
@@ -1,15 +1,144 @@
-import { View, Text, TextInput } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import Colors from "../constant/Colors";
+import { TypeList, WhenToTake } from "../constant/Options";
+import RNPickerSelect from "react-native-picker-select";
+import DropdownPicker from "./DropdownPicker";
 
 export default function AddMedicationForm() {
+  const [formData, setFormData] = useState();
+
+  const onHandleInputChange = (field, value) => {
+    setFormData((prev) => ({
+      ...prev,
+      [field]: value,
+    }));
+    console.log(formData);
+  };
   return (
-    <View className="p-6">
-      <Text className="text-[1.567rem] font-bold">Add New Medication</Text>
-      <View>
-        <Ionicons name="medkit-outline" size={24} color="black" />
-        <TextInput placeholder="Medicine Name" placeholderTextColor="gray" />
+    <View style={{ padding: 25 }}>
+      <Text style={styles.header}>Add New Medication</Text>
+      <View style={styles.inputGroup}>
+        <Ionicons
+          name="medkit-outline"
+          style={styles.icon}
+          size={24}
+          color="black"
+        />
+        <TextInput
+          placeholder="Medicine Name"
+          placeholderTextColor="#e5e7eb"
+          style={styles.textInput}
+          onChangeText={(value) => onHandleInputChange("name", value)}
+        />
+      </View>
+      {/* Type List */}
+      <FlatList
+        style={{
+          marginTop: 5,
+        }}
+        data={TypeList}
+        horizontal
+        renderItem={({ item, index }) => (
+          <TouchableOpacity
+            onPress={() => onHandleInputChange("type", item)}
+            style={[
+              styles.inputGroup,
+              { marginRight: 10 },
+              {
+                backgroundColor:
+                  item.name === formData?.type?.name ? Colors.PRIMARY : "white",
+              },
+            ]}
+          >
+            <Text
+              style={[
+                styles.typeText,
+                {
+                  color: item.name === formData?.type?.name ? "white" : "black",
+                },
+              ]}
+            >
+              {item?.name}
+            </Text>
+          </TouchableOpacity>
+        )}
+      />
+      {/* Dose */}
+      <View style={styles.inputGroup}>
+        <Ionicons
+          name="eyedrop-outline"
+          style={styles.icon}
+          size={24}
+          color="black"
+        />
+        <TextInput
+          placeholder="Dose Ex. 2, 5ml"
+          placeholderTextColor="#e5e7eb"
+          style={styles.textInput}
+          onChangeText={(value) => onHandleInputChange("dose", value)}
+        />
+      </View>
+      {/* When to take */}
+      <View style={[styles.inputGroup, { padding: 0 }]}>
+        <Ionicons
+          name="time-outline"
+          style={styles.icon}
+          size={24}
+          color="black"
+        />
+        <DropdownPicker
+          placeholder="Select and option"
+          value={formData?.when}
+          data={WhenToTake}
+          onChange={(itemValue, itemIndex) =>
+            onHandleInputChange("when", itemValue)
+          }
+        />
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  inputGroup: {
+    borderColor: Colors.LIGHT_GRAY_BORDER,
+    borderWidth: 1,
+    borderRadius: 8,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 10,
+    minHeight: 50,
+    backgroundColor: "white",
+  },
+  textInput: {
+    flex: 1,
+    marginLeft: 10,
+    fontSize: 16,
+  },
+  icon: {
+    color: Colors.PRIMARY,
+    borderRightWidth: 1,
+    paddingRight: 10,
+    paddingLeft: 12,
+    borderColor: Colors.LIGHT_GRAY_BORDER,
+  },
+  typeText: {
+    fontSize: 16,
+    paddingLeft: 12,
+    paddingRight: 12,
+  },
+});
---
+++ app/constant/Options.jsx
@@ -0,0 +1,37 @@
+export const TypeList = [
+  {
+    name: "Tablet",
+    icon: "https://cdn-icons-png.flaticon.com/128/2002/2002580.png",
+  },
+  {
+    name: "Capsules",
+    icon: "https://cdn-icons-png.flaticon.com/128/5419/5419454.png",
+  },
+  {
+    name: "Drops",
+    icon: "https://cdn-icons-png.flaticon.com/128/12285/12285293.png",
+  },
+  {
+    name: "Syrup",
+    icon: "https://cdn-icons-png.flaticon.com/128/11496/11496783.png",
+  },
+  {
+    name: "Injection",
+    icon: "https://cdn-icons-png.flaticon.com/128/3217/3217557.png",
+  },
+  {
+    name: "Other",
+    icon: "https://cdn-icons-png.flaticon.com/128/11496/11496770.png",
+  },
+];
+
+export const WhenToTake = [
+  { label: "Morning", value: "morning" },
+  { label: "Before Lunch", value: "before_lunch" },
+  { label: "After Lunch", value: "after_lunch" },
+  { label: "Afternoon", value: "afternoon" },
+  { label: "Evening", value: "evening" },
+  { label: "Before Dinner", value: "before_dinner" },
+  { label: "After Dinner", value: "after_dinner" },
+  { label: "Before Sleeping", value: "before_sleeping" },
+];
